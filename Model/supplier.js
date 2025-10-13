c227576bc076327815edede857a71c83c23b313a
Krish Ramena <krish.ramena24@vit.edu>
2025-10-13 21:08:18 +0530

    Update supplier.js

--- Model/supplier.js
+++ Model/supplier.js
@@ -4,12 +4,15 @@ const Schema = mongoose.Schema
 const supplierSchema = new Schema({
   name: { 
     type: String,
-    
      required: true 
     },
   contact: {
      type: String 
     },
+  Batch_number: {
+    type:String,
+    required: true
+  }
   address: {
      type: String 
     },
